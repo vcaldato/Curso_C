b528789b223fa5ddb186b1ce63ec9f2aae5db6e4
Vinicius Caldato <133283560+vcaldato@users.noreply.github.com>
2023-08-24 15:05:23 -0300

    Create Exerc3.c

---
+++ Exerc3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, quadrado;
+
+    printf("Digite um numero real: ");
+    scanf("%d", &num1);
+
+    quadrado = num1 * num1;
+
+    printf("O resultado é: %d",quadrado);
+    return 0;
+
+
+
+}
